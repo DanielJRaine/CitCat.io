173960d1fd11637754e9da34808696baa809ecb5
DanielJRaine <ley.danielj@gmail.com>
2016-09-07 13:55:32 -0400

    Add x and o players with turn swapping
    Created logic to keep track of player icon and turns.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,28 +7,6 @@
 require('./example');
 const boardEvents = require('./board/events');
 
-let game = {
-  id: 0,
-  cells: ["", "", "", "", "", "", "", "", ""],
-  over: false,
-  player_x: {
-    id: 0,
-    email: "x@x.com"
-  },
-  player_o: {
-    id: 1,
-    email: "o@o.com"
-  }
-};
-
-let player = {
-  id: 0,
-  email: 'x@x.com',
-  icon: 'x',
-  score: 0,
-  turn: true
-};
-
 $(() => {
     $('.col-md-2').on('click', boardEvents.onClickCell);
 });
