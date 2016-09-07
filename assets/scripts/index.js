e0efc4ff3bde7cfb9e564b2148f4bb471819ebef
DanielJRaine <ley.danielj@gmail.com>
2016-09-07 12:03:32 -0400

    Make cells clickable
    Separate asset functions into ui, events, and scripts files.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,12 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+const boardEvents = require('./board/events');
+// On document ready.  Explicitly does this on page load.
+// This is a callback [an anonymous function that takes no args]
+// All functions listed here will run when page is finished loading.
+$(() => {
+    $('.col-md-2').on('click', boardEvents.onClickCell);
+});
+;
