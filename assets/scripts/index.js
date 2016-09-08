38beec8f737a386452b59de6393159a33daee2d2
DanielJRaine <ley.danielj@gmail.com>
2016-09-08 11:54:26 -0400

    Add sign-up form
    Add html, css, and ajax event handlers to implement sign-up form.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,7 +6,9 @@
 // use require without a reference to ensure a file is bundled
 require('./example');
 const boardEvents = require('./board/events');
+const authEvents = require('./auth/events');
 
 $(() => {
     $('.col-md-2').on('click', boardEvents.onClickCell);
+    authEvents.addHandlers();
 });
