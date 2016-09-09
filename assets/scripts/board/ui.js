70e32831b6c47919236c2d50d09768386fc8f5ed
DanielJRaine <ley.danielj@gmail.com>
2016-09-09 13:51:50 -0400

    Refactor board/ui.js

--- assets/scripts/board/ui.js
+++ assets/scripts/board/ui.js
@@ -3,7 +3,6 @@ const app = require('../app.js');
 
 const xClick = () => {
   $(event.target).html('x');
-  
 };
 
 const oClick = () => {
@@ -11,18 +10,17 @@ const oClick = () => {
 };
 
 const turnError = () => {
-  alert('error in turns attribute');
+  console.log('error in turns attribute');
 };
 
 const createGameSuccess = (data) => {
-  console.log("create game success");
   console.log(data);
   app.game = data.game;
 };
 
 const createGameFail = () => {
   console.log("create game failed");
-}
+};
 
 const updateSuccess = (data) => {
   console.log(data);
